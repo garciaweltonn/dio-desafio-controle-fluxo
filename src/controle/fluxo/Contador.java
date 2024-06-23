22af0430da3828eee3ca4891353fff1b7d82e3e9
Welton <welton.garcia@sistematic.net.br>
2024-06-22 21:45:05 -0400

    commit inicial do projeto

--- src/controle/fluxo/Contador.java
+++ src/controle/fluxo/Contador.java
@@ -24,7 +24,6 @@ public class Contador {
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-
         // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
